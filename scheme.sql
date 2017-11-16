84a0dab66691285d8c75c1ac1cd140ee83bcd6ce
rendoir <up201503822@fe.up.pt>
2017-11-16 10:32:07 +0000

    Added database scheme

---
+++ scheme.sql
@@ -0,0 +1,35 @@
+PRAGMA FOREIGN_KEYS = ON;
+
+CREATE TABLE User (
+  username STRING PRIMARY KEY,
+  password STRING NOT NULL
+);
+
+CREATE TABLE Project (
+  title STRING PRIMARY KEY
+);
+
+CREATE TABLE Contributes (
+  user STRING REFERENCES User (username),
+  project STRING REFERENCES Project (title),
+  PRIMARY KEY (user, project)
+);
+
+CREATE TABLE TodoList (
+  title STRING,
+  category STRING,
+  color STRING,
+  project STRING REFERENCES Project (title),
+  PRIMARY KEY (project, title)
+);
+
+CREATE TABLE ListItem (
+  task STRING,
+  is_completed BOOLEAN DEFAULT(0),
+  due_date INTEGER,
+  color STRING,
+  todo_list STRING REFERENCES TodoList (title),
+  project STRING REFERENCES Project (title),
+  user STRING REFERENCES User (username),
+  PRIMARY KEY (task, project, todo_list, user)
+);
