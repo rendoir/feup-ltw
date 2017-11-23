1eea984c34156476af46a72c3044953f0feb50e7
rendoir <up201503822@fe.up.pt>
2017-11-23 09:57:59 +0000

    Cenas

--- php/database.php
+++ php/database.php
@@ -1,9 +1,22 @@
 <?php
   class DataBase {
-    private db;
+    private $db;
 
     public function __constructor($path) {
       $this->db = new PDO($path);
+    }
+
+    public function checkLogin($username, $password) {
+      $stmt = $this->db->prepare('SELECT *
+                                  FROM user
+                                  WHERE username = ? AND password = ?');
+      $stmt->execute(array($username, $password));
+      $user = $stmt->fetch();
+      print_r($user);
+    }
+
+    public function addUser($username, $password) {
+
     }
   }
 ?>
--- php/index.php
+++ php/index.php
@@ -1,5 +1,11 @@
 <?php
 include ('../html/header.html');
+include ('../php/database.php');
 include ('../html/login.html');
+
+$db = new DataBase('../database/todo.db');
+//$db->checkLogin('nao', 'existe');
+$db->checkLogin('timon', 'hakunamatata');
+
 include ('../html/footer.html');
 ?>
