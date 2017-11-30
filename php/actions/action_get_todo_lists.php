3681767c70433ad2a6f56f698781e0c5e4891fb0
rendoir <up201503822@fe.up.pt>
2017-11-30 17:45:04 +0000

    User can now add a todo list to a project

--- php/actions/action_get_todo_lists.php
+++ php/actions/action_get_todo_lists.php
@@ -4,9 +4,6 @@
 
   $project = $_POST["project"];
 
-  //TODO Remove this when we can add a todo list
-  DataBase::addToDoList("TITLE", $project, "CATEGORY", "COLOR");
-
   if($project !== null)
     echo json_encode(DataBase::getToDoListsOfProject($project));
   else echo json_encode(null);
