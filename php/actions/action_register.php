2da30018df25d12c9ffb80b37e07447be79f422d
rendoir <up201503822@fe.up.pt>
2017-12-02 15:11:01 +0000

    Added regular expressions to validate username and password

--- php/actions/action_register.php
+++ php/actions/action_register.php
@@ -5,12 +5,28 @@
   $username = $_POST['username'];
   $password = $_POST['password'];
 
+  function validUsername($username) {
+    return preg_match ('/^[a-zA-Z][a-zA-Z0-9_]{7,31}$/', $username);
+  }
+
+  function validPassword($password) {
+    $has_uppercase = preg_match('/[A-Z]/', $password);
+    $has_lowercase = preg_match('/[a-z]/', $password);
+    $has_number    = preg_match('/[0-9]/', $password);
+    $valid_length  = strlen($password) >= 8 && strlen($password) <= 64;
+    return $has_uppercase && $has_lowercase && $has_number && $valid_length;
+  }
+
   if($username === null || $password === null) {
     header('HTTP/1.0 403 Forbidden');
     exit();
-  }
-
-  if (DataBase::checkLogin($username, $password)) {
+  } else if(!validUsername($username)) {
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    die("Invalid username!");
+  } else if(!validPassword($password)) {
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    die("Invalid password!");
+  } if (DataBase::checkLogin($username, $password)) {
     header('Location: ' . $_SERVER['HTTP_REFERER']);
     die("Username already in use!");
   } else if(DataBase::addUser($username, $password)) {
