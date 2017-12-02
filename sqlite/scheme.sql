92849b96cb64ff2a43a84c45334144d7cdcd2a3a
rendoir <up201503822@fe.up.pt>
2017-12-02 12:50:46 +0000

    Added invitations to projects to the database scheme

--- sqlite/scheme.sql
+++ sqlite/scheme.sql
@@ -30,8 +30,15 @@ CREATE TABLE ListItem (
   task STRING,
   is_completed BOOLEAN DEFAULT(0),
   due_date INTEGER,
-  color STRING,
   todo_list INTEGER REFERENCES TodoList (list_id),
   user STRING REFERENCES User (username),
   UNIQUE(task, todo_list)
+);
+
+CREATE TABLE Invites (
+  invite_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  pending BOOLEAN DEFAULT(1),
+  accepted BOOLEAN,
+  user STRING REFERENCES User (username),
+  project STRING REFERENCES Project (title)
 );
