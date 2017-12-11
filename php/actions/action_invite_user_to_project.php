1f64bd39c2aa9ae27ddc16cb2496d9c1aeb30b66
rendoir <up201503822@fe.up.pt>
2017-12-11 22:15:27 +0000

    Finished refactoring actions

--- php/actions/action_invite_user_to_project.php
+++ php/actions/action_invite_user_to_project.php
@@ -3,15 +3,17 @@
   include_once('../database.php');
 
   $current_user = Session::getCurrentUser();
-  $user = $_POST["user"];
-  $project = $_POST["project"];
 
-  if($user !== null && $project !== null && $current_user !== null)
-    if(DataBase::isProjectManager($current_user, $project) &&
-       !DataBase::userContributesToProject($user, $project) &&
-       DataBase::userHasNoPendingInvite($user, $project))
-         echo json_encode(DataBase::inviteUserToProject($user, $project));
-    else echo json_encode(false);
-  else echo json_encode(false);
+  if(isset($_POST["user"]) &&
+     isset($_POST["project"]) &&
+     $current_user !== null) {
+       $user = $_POST["user"];
+       $project = $_POST["project"];
+       if(DataBase::isProjectManager($current_user, $project) &&
+          !DataBase::userContributesToProject($user, $project) &&
+          DataBase::userHasNoPendingInvite($user, $project))
+            echo json_encode(DataBase::inviteUserToProject($user, $project));
+       else echo json_encode(false);
+  } else echo json_encode(false);
 
 ?>
--- php/actions/action_set_completed_task.php
+++ php/actions/action_set_completed_task.php
@@ -2,13 +2,20 @@
   include_once('../session.php');
   include_once('../database.php');
 
-  $task = $_POST["task"];
-  $todo = $_POST["todo"];
-  $completed = $_POST["completed"];
-  $project = $_POST["project"];
+  $user = Session::getCurrentUser();
 
-  if($task !== null && $todo !== null && $completed !== null && $project !== null)
-    echo json_encode(DataBase::setCompletedListItem($task, $todo, $project, intval($completed)));
-  else echo json_encode(false);
+  if(isset($_POST["task"]) &&
+     isset($_POST["todo"]) &&
+     isset($_POST["completed"]) &&
+     isset($_POST["project"]) &&
+     $user !== null) {
+       $task = $_POST["task"];
+       $todo = $_POST["todo"];
+       $completed = $_POST["completed"];
+       $project = $_POST["project"];
+       if(DataBase::userContributesToProject($user, $project))
+         echo json_encode(DataBase::setCompletedListItem($task, $todo, $project, intval($completed)));
+       else echo json_encode(false);
+  } else echo json_encode(false);
 
 ?>
